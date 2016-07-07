41d836e4c9a33ede14af26a99b2444a2b282225e
jason-xu6 <xushoujie@139.com>
2016-07-07 17:50:06 +0800

    第三次
    后面几页的头部还有页尾写了

--- script/script.js
+++ script/script.js
@@ -1,4 +1,28 @@
-window.onload=function()
-{   
-	alert(document.body.clientHeight);
-};
+$(document).ready(function(){
+    $(function () {              
+            //绑定滚动条事件  
+              //绑定滚动条事件  
+            $(window).bind("scroll", function () {  
+                var sTop = $(window).scrollTop();  
+                sTop = parseInt(sTop);  
+                if (sTop >= 300) {  
+                    if (!$("#main-nav").is(":visible")) {  
+                        try {  
+                            $("#main-nav").slideDown();  
+                        } catch (e) {  
+                            $("#main-nav").show();  
+                        }                        
+                    }  
+                }  
+                else {  
+                    if ($("#main-nav").is(":visible")) {  
+                        try {  
+                            $("#main-nav").slideUp();  
+                        } catch (e) {  
+                            $("#main-nav").hide();  
+                        }                         
+                    }  
+                }   
+            });  
+        });
+});
