dbefea9f1be3181e18c1ccfdae2793b38f331ecd
jason-xu6 <xushoujie@139.com>
2016-07-27 22:34:47 +0800

    调整
    做了一些调整

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(function () {              
-            //绑定滚动条事件  
+              //绑定滚动条事件  
               //绑定滚动条事件  
             $(window).bind("scroll", function () {  
                 var sTop = $(window).scrollTop();  
@@ -104,29 +104,29 @@ $(document).ready(function(){
 
 /*右上角的nav*/
 $(document).ready(function(){
-    
+    var width=$(document.body).outerWidth(true);
     $("#main-nav div").mouseover(function()
         {   
              $("#nav-move").stop();  
-            $("#nav-move").animate({left: "1199px"});
+            $("#nav-move").animate({left: width-150});
            
         });
     $("#main-nav div").mouseout(function()
         {   
              $("#nav-move").stop();  
-            $("#nav-move").animate({left: "1349px"});
+            $("#nav-move").animate({left: width});
            
         });
     $("#nav-move").mouseover(function()
         {   
              $("#nav-move").stop();  
-            $("#nav-move").animate({left: "1199px"});
+            $("#nav-move").animate({left: width-150});
            
         });
     $("#nav-move").mouseout(function()
         {   
              $("#nav-move").stop();  
-            $("#nav-move").animate({left: "1349px"});
+            $("#nav-move").animate({left:width});
             
         });
 });
