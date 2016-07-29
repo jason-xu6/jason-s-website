4b72804cf9485c694143c9046142698455f8ecd6
jason-xu6 <xushoujie@139.com>
2016-07-29 22:35:21 +0800

    优化
    解决了nav-move的问题

--- script/script.js
+++ script/script.js
@@ -104,29 +104,28 @@ $(document).ready(function(){
 
 /*右上角的nav*/
 $(document).ready(function(){
-    var width=$(document.body).outerWidth(true);
     $("#main-nav div").mouseover(function()
         {   
              $("#nav-move").stop();  
-            $("#nav-move").animate({left: width-150});
+            $("#nav-move").animate({left: "1199px"});
            
         });
     $("#main-nav div").mouseout(function()
         {   
              $("#nav-move").stop();  
-            $("#nav-move").animate({left: width});
+            $("#nav-move").animate({left: "1349"});
            
         });
     $("#nav-move").mouseover(function()
         {   
              $("#nav-move").stop();  
-            $("#nav-move").animate({left: width-150});
+            $("#nav-move").animate({left: "1199px"});
            
         });
     $("#nav-move").mouseout(function()
         {   
              $("#nav-move").stop();  
-            $("#nav-move").animate({left:width});
+            $("#nav-move").animate({left:"1349"});
             
         });
 });
